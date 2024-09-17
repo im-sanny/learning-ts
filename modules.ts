305b945a1569ef854a66e763e44640759243748a
im-sanny <raselparvezsanny@gmail.com>
2024-09-17 21:23:54 +0600

    add: ts modules method added

---
+++ modules.ts
@@ -0,0 +1,8 @@
+module Utility {
+    export class useful {
+
+        modules(n: number) {
+            return n * 2;
+        }
+    }
+}
---
+++ util.ts
@@ -0,0 +1,4 @@
+///<reference path="modules.ts"/>
+var use = new Utility.useful();
+
+console.log(use.modules(9));
