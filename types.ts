a6fb38a05f472e4815ed7848f3327ce73fce87fd
im-sanny <raselparvezsanny@gmail.com>
2024-09-17 17:43:58 +0600

    add: added types file

---
+++ types.ts
@@ -0,0 +1,14 @@
+var winter: boolean = false;
+// isWinter = 123;
+
+var count: Number = 5;
+let lName: String = "Tom";
+
+var names: any[] = ['jon', 'ton', 'von', 4];
+
+enum Starks { tom, jerry, butch, bulldog };
+var thomas: Starks = Starks.tom;
+
+function getName(): void {
+    console.log('tom is coming');
+}
