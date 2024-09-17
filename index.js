be5919dfd9b5e2898d0b5709e2983dcc5a79e21c
im-sanny <raselparvezsanny@gmail.com>
2024-09-17 15:55:51 +0600

    add: added code with intensional error to check the js code in ts

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const menu = [
 ];
 
 const cashInRegister = 100;
+const nextOrderId = 1;
 const orderQue = [];
 
 /**
@@ -28,8 +29,36 @@ function addNewPizza(pizzaObj) {
 function placeOrder(pizzaName) {
   const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
   cashInRegister += selectedPizza.price;
-  const newOrder = { pizza: selectedPizza, status: "ordered" };
+  const newOrder = {
+    id: nextOrderId++,
+    pizza: selectedPizza,
+    status: "ordered",
+  };
   orderQue.push(newOrder);
   return newOrder;
 }
 
+/* 
+*Challenge: write another utility function, completeOrder, that takes an *orderId as a parameter 
+*finds the correct order in the orderQueue, and marks its status as "completed", for good measure, 
+*return the found order from the function.
+
+*Note: you'll need to ensure that we're adding IDs to our orders when we create new orders. You can use a global `nextOrderId` variable and increment it every time a new order is created to simulate real IDs being managed for us by a database
+*/
+
+function completeOrder(orderId) {
+  const order = orderQue.find((order) => order.id === order);
+  order.status = "completed";
+  return order;
+}
+
+addNewPizza({ name: "Chicken leg pizza", cost: 52 });
+addNewPizza({ name: "BBQ chicken leg", cost: 22 });
+addNewPizza({ name: "Rice pizza", cost: 12 });
+
+placeOrder("Chicken leg pizza");
+completeOrder("1");
+
+console.log("Menu:", menu);
+console.log("Cash in register:", cashInRegister);
+console.log("Oder queue:", orderQue);
