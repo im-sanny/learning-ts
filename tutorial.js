6abfc9f62fdbef29906ba1ad8a6042cf2845e278
im-sanny <raselparvezsanny@gmail.com>
2024-09-22 23:39:48 +0600

    add: tutorial file added

--- tutorial.js
+++ tutorial.js
@@ -1,14 +1,11 @@
 // let id: number = 5; //number type
 // let company: string = 'Google'; //string type
 // let isPublished: boolean = false; //boolean type
-
 // let ids: number[] = [1, 2, 3, 4, 5]; //this defines array of number type
 // let x: any = 'cats'; //any type
 // let xArr: any = ['Meow', 2, true]; //any type
-
-const concatenateValues = (a: string, b: string) => {
+var concatenateValues = function (a, b) {
     return a + b;
 };
-
-console.log(concatenateValues('Hello ', 'Meow'));
+console.log(concatenateValues('Hello', 'Meow'));
 console.log(concatenateValues('30', '50'));
